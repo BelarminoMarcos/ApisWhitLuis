5cc34f5548b07dc5cb129a52e5cd3138beaa1947
Belarmino <belarminom890@gmail.com>
2023-07-21 12:40:31 -0600

    CambioNuevo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using CursosLuis.Api.Helpers;
+using CursosLuis.Api.Model;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
@@ -15,6 +16,15 @@ builder.Services.AddSwaggerGen();
 //builder.Services.AddIdentityCore<Secion>()//este se comenta para que jale
 //.AddRoles<IdentityRole>();//Este se comenta
 
+builder.Services.AddIdentity<Secion, IdentityRole>()//Esta y_______________En la configuración del servicio, utiliza AddIdentity en lugar de AddIdentityCore para configurar la autenticación y autorización, y luego usa AddEntityFrameworkStores para especificar el contexto de la base de datos:
+    .AddEntityFrameworkStores<CursoDbContext>();//Esta debor borrarlas si no jala es mi implementacion
+builder.Services.AddAuthorization(o =>//esta Tambien se quita si no jala
+{
+    o.AddPolicy("SoloAdministrador", policy => policy.RequireRole("Administrador"));//esta igual se quita si no jala
+});
+
+
+
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
 //CONTROLA LOS ROLES DE LA SESSION DE UN USUARIO
 builder.Services.AgregarDependencias(builder.Configuration);
@@ -28,10 +38,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(o => {
         o.TokenValidationParameters = new()
         {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = false,
-            ValidateIssuerSigningKey = false,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
         };
     });
@@ -47,3 +57,4 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
